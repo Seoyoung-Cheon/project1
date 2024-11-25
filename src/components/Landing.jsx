44400f817fe91e517a4fb40af9ee9101d21b7f71
Seoyoung-Cheon <gu05087@gmail.com>
2024-11-25 18:15:44 +0900

    1125

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,6 +7,9 @@ const Landing = () => {
       <div className="w-full overflow-hidden h-[65vh] flex items-center justify-center relative">
         <div className="overlay w-full h-full bg-black z-10 left-0 top-0 absolute opacity-20"></div>
         <img src={landingImg} alt="" className="w-full" />
+        <div className="pop w-40 h-40 bg-white z-20 absolute flex items-center justify-center shadow-lg">
+          Pop Box
+        </div>
       </div>
     </div>
   );
