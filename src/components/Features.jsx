5ab23ab9d8c0b71de1030a6afa9b24134777140d
Seoyoung-Cheon <gu05087@gmail.com>
2024-11-26 17:56:43 +0900

    1126

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LandingSubBox from "./LandingSubBox";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div>
+      <LandingSubBox />
+    </div>
+  );
 };
 
 export default Features;
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import landingImg from "../assets/landing.avif";
+import { Link } from "react-router-dom";
+import LandingSubBox from "./LandingSubBox";
 
 const Landing = () => {
   return (
-    <div className="w-full">
+    <div className="w-full relative">
       <div className="w-full overflow-hidden h-[65vh] flex items-center justify-center relative">
         <div className="overlay w-full h-full bg-black z-10 left-0 top-0 absolute opacity-20"></div>
-        <img src={landingImg} alt="" className="w-full" />
-        <div className="pop w-40 h-40 bg-white z-20 absolute flex items-center justify-center shadow-lg">
-          Pop Box
+        <div className="slogan-box absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col items-center gap-4">
+          <h2 className="text-3xl text-white font-semibold ">
+            Welcome To The Met
+          </h2>
+          <Link
+            to="/"
+            className="font-semibold text-white border border-solid border-white px-4 py-2 rounded-sm transition-all transition-duration-300 hover:bg-white hover:text-gray-500"
+          >
+            Plan your visit
+          </Link>
         </div>
+        <img src={landingImg} alt="" className="w-full" />
+      </div>
+      <div className="bottom-box absolute bottom-[-20%] z-50 w-[60%] transform -translate-x-1/2 left-1/2 border rounded-sm">
+        <LandingSubBox />
       </div>
     </div>
   );
